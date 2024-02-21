17bde2f9190f7eab08e9bbeb7d37db36a56ba59d
rdunder <r.dunder@gmail.com>
2024-02-21 22:37:16 +0100

    working on activities

--- MudBlazorServerWebApp/Data/PlannedActivity.cs
+++ MudBlazorServerWebApp/Data/PlannedActivity.cs
@@ -5,7 +5,7 @@ namespace MudBlazorServerWebApp.Data
     public class PlannedActivity
     {
         public int Id { get; set; }
-        public DateTime DateTime { get; set; }
+        public DateTime? DateTime { get; set; }
         public string? Activity { get; set; }
     }
 }
