3fef440a4cc943ec16ea325987b9c7b95cc4127f
rdunder <r.dunder@gmail.com>
2024-02-21 10:16:16 +0100

    usermanagement done, starting on planned activities

--- MudBlazorServerWebApp/Data/ApplicationDbContext.cs
+++ MudBlazorServerWebApp/Data/ApplicationDbContext.cs
@@ -5,5 +5,8 @@ namespace MudBlazorServerWebApp.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+
+        public DbSet<PlannedActivity> PlannedActivities { get; set; }
+        
     }
 }
---
+++ MudBlazorServerWebApp/Data/PlannedActivity.cs
@@ -0,0 +1,8 @@
+﻿namespace MudBlazorServerWebApp.Data
+{
+    public class PlannedActivity
+    {
+        public DateTime? DateTime { get; set; }
+        public string? Activity { get; set; }
+    }
+}
